c8a681fa32d85b2dcb85e7193e87eb304411def2
Usuario <usuario@example.com>
2026-02-11 08:04:14 -0300

    fix railway port

--- backend/app.py
+++ backend/app.py
@@ -138,4 +138,6 @@ def add_header(response):
 # START
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
+
